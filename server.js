f8068733545b2aac7367dedb52abf188be4d2a6f
Joe LoPresti <jlopresti87@gmail.com>
2019-01-24 21:06:35 -0500

    Configure express-fileupload

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(fileUpload());
 app.use('/', express.static('./public'));
 
 app.post('/api/fileanalyze', (req,res) => {
-  let fileName = req.files.upload;
+  let { name, mimetype, data } = req.files.upfile;
   
-  res.send(fileName);
+  res.send({ name, type: mimetype, size: `${parseFloat(data.length).toLocaleString()} bytes`});
 });
 
 app.listen(PORT);
